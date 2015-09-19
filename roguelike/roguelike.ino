e3ba7a2c5f3732edeaaef4ca510cd60956ab1fcc
Edward Dibbs <EdDibbs@gmail.com>
2015-09-19 12:54:45 -0700

    Renamed main to roguelike to fix issue where program wouldn't compile Added code to test joystick Added Scripts folder which will hold scripts to prepare files for compilation

---
+++ roguelike/roguelike.ino
@@ -0,0 +1,19 @@
+int UpDown    = 0;
+int LeftRight = 0;
+
+
+void setup() {  
+  //setup serial communication
+  Serial.begin(9600);
+}
+
+void loop() {
+  UpDown = analogRead(A7);
+  LeftRight = analogRead(A6);
+  Serial.print("UpDown = ");
+  Serial.print(UpDown, DEC);
+  Serial.print(", LeftRight = ");
+  Serial.println(LeftRight, DEC);
+  delay(200);
+  
+}
