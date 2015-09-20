b62f50dd7fcdcf73207b7e477e066e6e9c7f891d
Edward Dibbs <EdDibbs@gmail.com>
2015-09-19 20:10:48 -0700

    added SpriteDefinitions.h to contain all the code for the sprites created the ConvertImages.ipynb python notebook that can convert images from png to the format I need changed the player representation from a pixel to a sprite

--- roguelike/roguelike.ino
+++ roguelike/roguelike.ino
@@ -1,6 +1,8 @@
 #include <Adafruit_GFX.h>   //Core graphics library
 #include <Adafruit_ST7735.h> //hardware-specific library
+#include <avr/pgmspace.h>
 #include <SPI.h> //??
+#include "SpriteDefinitions.h"
 
 // For the breakout, you can use any 2 or 3 pins
 // These pins will also work for the 1.8" TFT shield
@@ -15,16 +17,15 @@
 // to use the microSD card (see the image drawing example)
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
 
-// Option 2: use any pins but a little slower!
-#define TFT_SCLK 13   // set these to be whatever pins you like!
-#define TFT_MOSI 11   // set these to be whatever pins you like!
-//Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
-
 
 int playerX;
 int playerY;
-int playerSpeed = 5;
-
+uint16_t playerWidth;
+uint16_t playerHeight;
+int lastPlayerX = -1;
+int lastPlayerY = -1;
+int playerSpeed = 10;
+uint16_t bgcolor = ST7735_BLACK;
 
 //clock stuff
 long startTime;
@@ -32,57 +33,90 @@ int count = 0;
 int lastFPS = 0;
 
 void setup() {  
-   // Use this initializer if you're using a 1.8" TFT
+  //setup serial communication
+  Serial.begin(9600);
+  
+  // Use this initializer if you're using a 1.8" TFT
   tft.initR(INITR_BLACKTAB);   // initialize a ST7735S chip, black tab
   tft.setRotation(1);
-  tft.fillScreen(ST7735_BLACK);
+  tft.fillScreen(bgcolor);
 
-  playerX = tft.width() / 2;
-  playerY = tft.height() / 2;
+  playerWidth = (uint16_t)pgm_read_word_near(PC_WIDTH);
+  playerHeight = (uint16_t)pgm_read_word_near(PC_HEIGHT);
+  
+  playerX = (tft.width() / 2) - (playerWidth / 2);
+  playerY = (tft.height() / 2) - (playerHeight / 2);
 
   startTime = millis();
-  //setup serial communication
-  //Serial.begin(9600);
+}
+
+void clearSprite()
+{
+  tft.fillRect(lastPlayerX, lastPlayerY, playerWidth, playerHeight, bgcolor);
+}
+
+void drawSprite()
+{
+  for ( int x = 0; x < playerWidth; x++)
+  {
+    for (int y = 0; y < playerHeight; y++)
+    {
+      int index = (y * playerWidth) + x;
+
+      int value = pgm_read_word_near(PC + index);
+      tft.drawPixel(playerX + x, playerY + y, value);
+    }
+  }
 }
 
 void movePlayer()
 {
   float UpDown = ((analogRead(A7) / 1020.0) - 0.5) * -1;
   float LeftRight = ((analogRead(A6) / 1020.0) - 0.5 ) * -1;
-  
+
+  lastPlayerX = playerX;
   playerX = playerX + (int)(playerSpeed * LeftRight);
-  if (playerX > tft.width()) playerX = tft.width();
+  if (playerX + playerWidth > tft.width()) playerX = tft.width() - playerWidth;
   if (playerX < 0) playerX = 0;
-  
+
+  lastPlayerY = playerY;
   playerY = playerY + (int)(playerSpeed * UpDown);
-  if (playerY > tft.height()) playerY = tft.height();
+  if (playerY + playerHeight > tft.height()) playerY = tft.height() - playerHeight;
   if (playerY < 0) playerY = 0;
+
+//  Serial.print("X: ");
+//  Serial.print(playerX, DEC);
+//  Serial.print("  Y: ");
+//  Serial.println(playerY, DEC); 
+  
+  if (lastPlayerX != playerX || lastPlayerY != playerY)
+  {
+    clearSprite();
+    drawSprite();    
+  }
 }
 
 void printFPS(int fps, uint64_t color)
 {
   tft.setCursor(1,1);
   tft.setTextColor(color);
-  tft.setTextSize(2);
+  tft.setTextSize(1);
   tft.print(fps, DEC);
 }
 
 void loop() {  
-  printFPS(lastFPS, ST7735_WHITE);
-  
   count++;
   if (millis() - startTime >= 1000)
   {
-    printFPS(lastFPS, ST7735_BLACK);
+    printFPS(lastFPS, bgcolor);
     lastFPS = count;
+    printFPS(lastFPS, ST7735_WHITE);
     
     startTime = millis();
     count = 0;
   }
   
-  tft.drawPixel(playerX, playerY, ST7735_BLACK);
+  
   movePlayer();   
- 
-  tft.drawPixel(playerX, playerY, ST7735_WHITE);
-  //delay(10);  
+   //delay(10);  
 }
